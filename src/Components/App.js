f25512cdc5df140e215ed748b8245edcc0903305
curtis <chubbard022@gmail.com>
2020-04-04 08:10:11 -0700

    got basic dashboard layout

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles.css";
-import { Dashboard } from "./Dashboard";
+import { Dashboard } from "./Dashboard/Dashboard";
 
 export default function App() {
   return (
---
+++ src/Components/Dashboard/UserAccount.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "../../styles.css"
+
+export const UserAccount = () =>{
+    return(
+        <div className="DashboardBlock">
+           User Account
+        </div>
+    )
+}
