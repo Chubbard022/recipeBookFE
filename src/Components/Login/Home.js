0d50b04b8dc2e4515e33f34fcefc6702c16a6657
curtis <chubbard022@gmail.com>
2020-04-28 05:30:12 -0700

    adding register page if user has not made an account yet

--- src/Components/Login/Home.js
+++ src/Components/Login/Home.js
@@ -2,14 +2,8 @@ import React, {Component} from "react"
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import axios from "axios"
 
 import "../../styles.css"
-
-
-
-
-
 
 
 
@@ -31,21 +25,19 @@ export class Home extends Component{
     render(){
         return(
             <div>
-                <header>
                     <Typography variant="h1" component="h2" gutterBottom>
                         RecipeMaker
                     </Typography>
-                    <body>
                         <form className="loginBox">
                             <TextField required id="standard-required" label="Username" />
                             <br/>
                             <TextField style={{paddingBottom: "10%"}} required id="standard-required" label="Password" />
                             <br/>
                             <Button variant="contained" color="primary" onClick={this.handleSubmit}>Submit</Button>
-                            <p>if not registered, please click <a onClick={()=>this.props.history.push("/register")}>here</a></p>
+                            <p>if not registered, please click 
+                                <a className="registerRedirect" onClick={()=>this.props.history.push("/register")}> here</a>
+                            </p>
                         </form>
-                    </body>
-                </header>
             </div>
         )
     }
