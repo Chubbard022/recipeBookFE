63320e85013a45adc46eaca18ed0ab359b502015
curtis <chubbard022@gmail.com>
2020-04-29 09:31:12 -0700

    adding testing libraries and starting first tests on home component

---
+++ src/Components/Login/Home.test.js
@@ -0,0 +1,16 @@
+const Home = require("./Home")
+
+
+describe('Addition', () => {
+    it('knows that 2 and 2 make 4', () => {
+      expect(2 + 2).toBe(4);
+    });
+});
+
+describe("Login",()=>{
+    describe("When given username and password",()=>{
+        it("should let the user know if they are allowed in or not",()=>{
+            
+        })
+    })
+})
