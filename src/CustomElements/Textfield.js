78e9d78ef98ee81e8c1183322918370ba3282e21
curtis <chubbard022@gmail.com>
2020-05-01 06:24:35 -0700

    adding custom styled components

---
+++ src/CustomElements/Textfield.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+
+
+export const TextField = styled.input`
+    width: 42%;
+    margin: 0 auto;
+    font-size: 25px;
+    border: 1px solid white;
+    border-bottom: 1px solid grey;
+    margin: 1%
+`
