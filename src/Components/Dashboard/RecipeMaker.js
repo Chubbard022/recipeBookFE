fa8fd46e48f68ab00332dd9c57fd4d68d852d09f
Chubbard022 <chubbard022@gmail.com>
2020-05-04 06:05:46 -0700

    adding stuff to recipe

--- src/Components/Dashboard/RecipeMaker.js
+++ src/Components/Dashboard/RecipeMaker.js
@@ -1,12 +1,64 @@
-import React from "react"
+import React,{Component} from "react"
+import axios from "axios"
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 
 import "../../styles.css"
 
 
-export const RecipeMaker = () =>{
-    return(
-        <div>
-            Make Recipe
-        </div>
-    )
+export class RecipeMaker extends Component{
+    constructor(){
+        super()
+        this.state={
+            form: {
+                name: "",
+                ingredients: [],
+                instructions: "",
+                cookingTime: ""
+            }
+        }
+    }
+
+    handleSubmit = () =>{
+        
+    }
+
+
+    render(){
+        return(
+            <div className="recipeForm">
+                <form>
+                    <TextField
+                    id="standard-name"
+                    label="recipe Name"
+                    // value={0}
+                    // onChange={handleChange('name')}
+                    margin="normal"
+                    />
+
+                    <br/>
+
+                    <TextField
+                    id="standard-name"
+                    label="Ingredient List"
+                    // value={0}
+                    // onChange={handleChange('name')}
+                    margin="normal"
+                    />
+
+                    <br/>
+
+                    <TextField
+                    id="standard-name"
+                    label="Instructions"
+                    // value={0}
+                    // onChange={handleChange('name')}
+                    margin="normal"
+                    />
+                </form> 
+                <Button variant="contained" color="primary" onClick={this.handleSubmit}>Create Recipe</Button>
+            </div>
+        )
+    }
 }
