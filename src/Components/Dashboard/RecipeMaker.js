0e4e383ad7b186a45274300b99b4b3b082626a3a
curtis <chubbard022@gmail.com>
2020-05-14 08:22:55 -0700

    getting styling done for recipe maker

--- src/Components/Dashboard/RecipeMaker.js
+++ src/Components/Dashboard/RecipeMaker.js
@@ -1,33 +1,58 @@
 import React,{Component} from "react"
-import axios from "axios"
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 
 import "../../styles.css"
 
+//overriding default theme 
+const theme = createMuiTheme({
+    overrides: {
+      // Style sheet name ⚛
+      MuiInput: {
+        // Name of the rule
+        input: {
+          // Some CSS
+          height: "200",
+          fontSize: "3em",
+          padding:"10%",
+          fontSize: "25px"
+        },
+      },
+      MuiButton: {
+          text: {
+              border:"3px solid red"
+          }
+      }
+    },
+  });
 
 export class RecipeMaker extends Component{
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
         this.state={
             form: {
                 name: "",
                 ingredients: [],
                 instructions: "",
-                cookingTime: ""
+                cookingTime: "",
             }
         }
-    }
-
-    handleSubmit = () =>{
-        axios.get()
     }
 
 
     render(){
         return(
+            <div>
+            <Button 
+                onClick={()=>this.props.history.push("/dashboard")}
+                variant="contained" 
+                color="primary" 
+            >Back To Dashboard</Button>
+
             <div className="recipeForm">
+             <ThemeProvider theme={theme}>
                 <form>
                     <TextField
                     id="standard-name"
@@ -35,6 +60,7 @@ export class RecipeMaker extends Component{
                     // value={0}
                     // onChange={handleChange('name')}
                     margin="normal"
+                    className="formInputField"
                     />
 
                     <br/>
@@ -45,20 +71,28 @@ export class RecipeMaker extends Component{
                     // value={0}
                     // onChange={handleChange('name')}
                     margin="normal"
+                    className="formInputField"
                     />
 
                     <br/>
 
                     <TextField
-                    id="standard-name"
                     label="Instructions"
                     // value={0}
                     // onChange={handleChange('name')}
                     margin="normal"
+                    className="formInputField"
                     />
                 </form> 
-                <Button variant="contained" color="primary" onClick={this.handleSubmit}>Create Recipe</Button>
+                <Button 
+                    variant="contained" 
+                    color="primary" 
+                    onClick={this.handleSubmit}
+                    style={{marginTop: "5%",fontSize:"16px"}}
+                >Create Recipe</Button>
+                </ThemeProvider>
             </div>
+        </div>
         )
     }
 }
