8e702cbf63505a0f51bb6d739b2dde78c2fe1e5c
curtis <chubbard022@gmail.com>
2020-05-15 09:03:44 -0700

    creating functionality for creating recipes

--- src/Components/Dashboard/RecipeMaker.js
+++ src/Components/Dashboard/RecipeMaker.js
@@ -5,7 +5,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
-import {createRecipe} from "../../Actions/recipe"
+import {createRecipe, editRecipe, deleteRecipe,getRecipes} from "../../Actions/recipe"
 import "../../styles.css"
 
 //overriding default theme 
@@ -34,17 +34,55 @@ class RecipeMaker extends Component{
     constructor(props){
         super(props)
         this.state={
-            form: {
+            form : {
                 name: "",
-                ingredients: [],
+                ingredients: "",
                 instructions: "",
-                cookingTime: "",
+                username : ""
             }
         }
     }
-    render(){
-        return(
-            <div>
+    componentDidMount(){
+       this.setState({
+           form: {
+               ...this.state.form,
+               username: this.props.username
+           }
+       })
+    }
+
+    handleChange = (e) => {
+        this.setState({ 
+            form:{
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })  
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault()
+        if((this.state.form.name !== "") && (this.state.form.instructions !== "") && (this.state.form.ingredients !== "")){
+            this.props.createRecipe(this.state.form)
+            console.log(this.state.form)
+            console.log(this.props)
+            this.setState({ 
+                form : {
+                    ...this.state.form,
+                    name: "",
+                    ingredients: "",
+                    instructions: "",
+                }
+            })  
+        }
+
+    }
+
+
+
+render(){
+    return(
+        <div>
             <Button 
                 onClick={()=>this.props.history.push("/dashboard")}
                 variant="contained" 
@@ -52,44 +90,44 @@ class RecipeMaker extends Component{
             >Back To Dashboard</Button>
 
             <div className="recipeForm">
-             <ThemeProvider theme={theme}>
-                <form>
-                    <TextField
-                    id="standard-name"
-                    label="recipe Name"
-                    // value={0}
-                    // onChange={handleChange('name')}
-                    margin="normal"
-                    className="formInputField"
-                    />
-
-                    <br/>
-
-                    <TextField
-                    id="standard-name"
-                    label="Ingredient List"
-                    // value={0}
-                    // onChange={handleChange('name')}
-                    margin="normal"
-                    className="formInputField"
-                    />
-
-                    <br/>
-
-                    <TextField
-                    label="Instructions"
-                    // value={0}
-                    // onChange={handleChange('name')}
-                    margin="normal"
-                    className="formInputField"
-                    />
-                </form> 
-                <Button 
-                    variant="contained" 
-                    color="primary" 
-                    onClick={this.handleSubmit}
-                    style={{marginTop: "5%",fontSize:"16px"}}
-                >Create Recipe</Button>
+                <ThemeProvider theme={theme}>
+                    <form>
+                        <TextField
+                            id="standard-name"
+                            label="recipe Name"
+                            name="name"
+                            value={this.state.form.name}
+                            onChange={this.handleChange}
+                            margin="normal"
+                            className="formInputField"
+                        />
+                        <br/>
+                        <TextField
+                            id="standard-name"
+                            label="Ingredient List"
+                            name="ingredients"
+                            value={this.state.form.ingredients}
+                            onChange={this.handleChange}
+                            margin="normal"
+                            className="formInputField"
+                        />
+                        <br/>
+                        <TextField
+                            id="standard-name"                        
+                            label="Instructions"
+                            name="instructions"
+                            value={this.state.form.instructions}
+                            onChange={this.handleChange}
+                            margin="normal"
+                            className="formInputField"
+                        />
+                    </form> 
+                    <Button 
+                        variant="contained" 
+                        color="primary" 
+                        onClick={this.handleSubmit}
+                        style={{marginTop: "5%",fontSize:"16px"}}
+                    >Create Recipe</Button>
                 </ThemeProvider>
             </div>
         </div>
@@ -98,9 +136,9 @@ class RecipeMaker extends Component{
 }
 
 const mapStateToProps = (state) =>({
-
+    username: state.username
 })
 export default connect(
     mapStateToProps,
-    {createRecipe}
+    {createRecipe,editRecipe,deleteRecipe,getRecipes}
 )(RecipeMaker);
