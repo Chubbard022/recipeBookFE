c533142e8b65276fb8100be216bc5c23d01feb91
curtis <chubbard022@gmail.com>
2020-05-18 07:11:38 -0700

    adding basics of layout

--- src/Components/Dashboard/UserAccount.js
+++ src/Components/Dashboard/UserAccount.js
@@ -1,11 +1,40 @@
-import React from "react"
+import React,{Component} from "react"
+import {connect} from "react-redux"
+import Button from "@material-ui/core/Button"
 
+import {getRecipes} from "../../Actions/recipe"
 import "../../styles.css"
 
-export const UserAccount = () =>{
-    return(
-        <div>
-           User Account
-        </div>
-    )
+
+function UserAccount (props){
+    console.log("PROPS",props)
+        return(
+            <div>
+                <Button
+                    onClick={()=>props.history.push("/dashboard")}
+                    variant="contained" 
+                    color="primary" 
+                >Back To Dashboard</Button>
+            <div>
+               { props.recipes.map(recipe=>{
+                    return <div className="recipeListDisplay">
+                        <p>{recipe.name}</p>
+                        <p>{recipe.ingredients}</p>
+                        <p>{recipe.instructions}</p>
+                    </div>
+                })}
+            </div>
+
+
+            </div>
+        )
 }
+
+const mapStateToProps = (state) =>({
+    recipes: state.recipes
+})
+
+export default connect(
+    mapStateToProps,
+    (getRecipes)
+)(UserAccount)
