a35528f377ade123ba819b88d5af00ef2de8c8db
curtis <chubbard022@gmail.com>
2020-05-19 08:21:49 -0700

    filtering recipes to each users personal recipes

--- src/Components/Dashboard/UserAccount.js
+++ src/Components/Dashboard/UserAccount.js
@@ -4,34 +4,50 @@ import Button from "@material-ui/core/Button"
 
 import {getRecipes} from "../../Actions/recipe"
 import "../../styles.css"
+import { filter } from "dom-helpers";
 
 
-function UserAccount (props){
-    console.log("PROPS",props)
+class UserAccount extends Component{
+        constructor(props){
+            super(props)
+            this.state={
+                personalRecipes: []
+            }
+        }
+        componentDidMount() {
+            let filterRecipes = this.props.recipes.filter(recipe=> recipe.username == this.props.username)
+            this.setState({...this.state,personalRecipes:filterRecipes})
+        }
+        render(){
+            console.log(this.state)
         return(
             <div>
                 <Button
-                    onClick={()=>props.history.push("/dashboard")}
+                    onClick={()=>this.props.history.push("/dashboard")}
                     variant="contained" 
                     color="primary" 
                 >Back To Dashboard</Button>
-            <div>
-               { props.recipes.map(recipe=>{
-                    return <div className="recipeListDisplay">
-                        <p>{recipe.name}</p>
-                        <p>{recipe.ingredients}</p>
-                        <p>{recipe.instructions}</p>
-                    </div>
+            {/* <div>
+                { props.recipes.filter(recipe=>{
+                    if(recipe.username == props.username){
+                        return( <div className="recipeListDisplay">
+                            <p>{recipe.name}</p>
+                            <p>{recipe.ingredients}</p>
+                            <p>{recipe.instructions}</p>
+                        </div>)
+                    }
                 })}
-            </div>
+            </div> */}
 
 
             </div>
         )
+    }
 }
 
 const mapStateToProps = (state) =>({
-    recipes: state.recipes
+    recipes: state.recipes,
+    username: state.username
 })
 
 export default connect(
