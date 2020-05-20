a2ba9cf4d1f2553d48fa84b95a3854f083c7375d
curtis <chubbard022@gmail.com>
2020-05-20 08:38:37 -0700

    adding logic to send specific recipe to edited page when clicked

--- src/Components/Dashboard/UserAccount.js
+++ src/Components/Dashboard/UserAccount.js
@@ -1,23 +1,45 @@
 import React,{Component} from "react"
 import {connect} from "react-redux"
+import {Link} from 'react-router-dom';
 import Button from "@material-ui/core/Button"
 
 import {getRecipes} from "../../Actions/recipe"
+import EditUserAccount from "./EditUserAccount"
 import "../../styles.css"
-import { filter } from "dom-helpers";
+
 
 
 class UserAccount extends Component{
         constructor(props){
             super(props)
             this.state={
-                personalRecipes: []
+                personalRecipes: [],
+                editRecipe: [],
+                edit: false
             }
         }
         componentDidMount() {
             let filterRecipes = this.props.recipes.filter(recipe=> recipe.username == this.props.username)
             this.setState({...this.state,personalRecipes:filterRecipes})
         }
+        handleSelectEdit(recipe){
+            let temp = []
+            temp.push(recipe)
+            this.setState({
+                ...this.state,
+                editRecipe:temp,
+                edit: !this.state.edit
+            })
+            
+        }
+        handleEditChange = () =>{
+            console.log("WORKS")
+        }
+        handleFinishEdit = () =>{
+            let temp = []
+            this.setState({...this.state, editRecipe:temp})
+        }
+
         render(){
             console.log(this.state)
         return(
@@ -27,19 +49,34 @@ class UserAccount extends Component{
                     variant="contained" 
                     color="primary" 
                 >Back To Dashboard</Button>
-            {/* <div>
-                { props.recipes.filter(recipe=>{
-                    if(recipe.username == props.username){
-                        return( <div className="recipeListDisplay">
+
+                <div className={this.state.editRecipe[0]? "vanish" : null}>
+                    {this.state.personalRecipes.map((recipe,index)=>{
+                        return <div className="recipeListDisplay" key={index}> 
                             <p>{recipe.name}</p>
-                            <p>{recipe.ingredients}</p>
                             <p>{recipe.instructions}</p>
-                        </div>)
-                    }
-                })}
-            </div> */}
+                            <p>{recipe.ingredients}</p>
 
-
+                            <Button 
+                                onClick={()=>this.handleSelectEdit(recipe)}
+                                variant="contained" 
+                                color="primary" 
+                                style={{fontSize: "8px",float:"right"}}
+                                >Edit
+                            </Button>
+                        </div>
+                    })}
+                </div>
+                {this.state.editRecipe[0]?
+                    <EditUserAccount 
+                        editRecipe={this.state.editRecipe[0]} 
+                        edit={this.state.edit}
+                        handleEditChange={this.handleEditChange}
+                        handleFinishEdit={this.handleFinishEdit}
+                    />
+                    :
+                    null
+                }
             </div>
         )
     }
