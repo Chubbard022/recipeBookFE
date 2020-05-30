331ff74f9199f8a3d4326c52688e8aac421b98ed
curtis <chubbard022@gmail.com>
2020-05-30 06:37:42 -0700

    setting up barebone stateful react file thats using redux

--- src/Components/Dashboard/Inspiration.js
+++ src/Components/Dashboard/Inspiration.js
@@ -1,9 +1,51 @@
-import React from "react"
+import React, {Component} from "react"
+import {connect} from "react-redux"
+import Button from "@material-ui/core/Button"
 
+
+import {getInspiration} from "../../Actions/recipe"
 import "../../styles.css"
 
-export const Inspiration = () =>{
-    return(
-        <div > Get Inspiration</div>
-    )
+class Inspiration extends Component{
+    constructor(){
+        super()
+        this.state = {
+            recipe: [],
+            olderInspirationRecipes: []
+        }
+    }
+
+    componentWillMount(){
+        let test = this.props.getInspiration()
+        console.log(test)
+        // this.setState({
+        //     ...state,
+        //     recipe: this.props.getInspiration
+        // })
+    }
+
+
+    render(){
+        return(
+            <div> 
+                <Button 
+                    onClick={()=>this.props.history.push("/dashboard")}
+                    variant="contained" 
+                    color="primary" 
+                >Back To Dashboard</Button>
+                <div>
+                    
+                </div>
+            </div>
+        )
+    }
 }
+
+const mapStateToProps = (state) =>({
+    inspirationRecipes: state.inspirationRecipes
+})
+
+export default connect(
+    mapStateToProps,
+    {getInspiration}
+)(Inspiration)
