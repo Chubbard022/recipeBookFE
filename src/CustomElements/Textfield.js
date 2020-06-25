b249ef33cbcff7e59b911aa3997ecd3847458bb8
curtis <chubbard022@gmail.com>
2020-06-25 06:17:39 -0700

    cleaning up spacing of text boxes

--- src/CustomElements/Textfield.js
+++ src/CustomElements/Textfield.js
@@ -7,7 +7,9 @@ export const TextField = styled.input`
     width: 42%;
     margin: 0 auto;
     font-size: 25px;
+    box-sizing: border-box;
     border: 1px solid white;
     border-bottom: 1px solid grey;
-    margin: 1%
+    margin-bottom: 10%;
+    display:block;
 `
