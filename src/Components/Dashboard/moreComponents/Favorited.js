6eaf56f7daeb69796badbac961d8c01ad07c096f
curtis <chubbard022@gmail.com>
2020-07-21 08:27:52 -0700

    working on getting button to click when favorited

--- src/Components/Dashboard/moreComponents/Favorited.js
+++ src/Components/Dashboard/moreComponents/Favorited.js
@@ -1,11 +1,19 @@
-import React from "react"
+import React, {useState} from "react"
+import star from "../../../images/star.png"
+import favorite from "../../../images/favorite.png"
+import "../LinksWithinDash/linksWithinDash.css"
 
+
+const clickedStar = () =>{
+    console.log("working")
+}
 
 const Favorited = (props) =>{
-
+    const [clicked,setClicked] = useState(false)
     return(
         <div>
             Favorited Recipes
+            <img className="favoriteRecipe" onClick={clickedStar} src={star} alt="bordered star"/>
         </div>
     )
 }
